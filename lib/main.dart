293916f7474478f5e2b12543ffda4f80ac18eba9
unknown <wael.kalakech@hotmail.com>
2024-02-11 22:21:58 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:finance/pages/home.dart';
+import 'package:finance/pages/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FinanceApp());
+}
+
+class FinanceApp extends StatelessWidget {
+  const FinanceApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: Splashscreen(),
+    );
+  }
+}
---
+++ lib/pages/splashscreen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset("assets/svg/logo.svg"),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Finance",
+              style: TextStyle(fontSize: 30, color: Colors.green),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
